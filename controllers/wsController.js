0e3add6822090195bd2e7b8ddd7d3b9827bea828
Esteban Cortina <estebannsebastian@gmail.com>
2023-05-29 01:48:27 -0600

    Add(Alexa): Reconoce Alexa

--- controllers/wsController.js
+++ controllers/wsController.js
@@ -10,49 +10,59 @@ module.exports = (ws) => {
   console.log("Nueva conexion");
   conexiones.push(ws);
   client.on('message', (topic, message) => {
-
     const messageObj = JSON.parse(message);
+    console.log(messageObj);
+    if (messageObj[messageObj.length - 1].type === 2) {
+      console.log("sexo");
+      ws.send(JSON.stringify({ "type": "sexo" }));
+    } else {
 
-    messageObj.forEach(sensor => {
-      const fechaActual = new Date();
-      const offset = (fechaActual.getTimezoneOffset() + 60) * 60 * 1000;
-      const fechaLocal = new Date(fechaActual.getTime() - offset);
-      const fechaISO = fechaLocal.toISOString();
+      messageObj.forEach(sensor => {
+        const fechaActual = new Date();
+        const offset = (fechaActual.getTimezoneOffset() + 60) * 60 * 1000;
+        const fechaLocal = new Date(fechaActual.getTime() - offset);
+        const fechaISO = fechaLocal.toISOString();
 
-      //Agregar fecha al objeto de respuesta.
-      sensor.timeStamp = fechaLocal;
+        //Agregar fecha al objeto de respuesta.
+        sensor.timeStamp = fechaLocal;
+        (async () => {
+          try {
+            const nuevoDocumento = new Data({
+              timeStamp: fechaISO,
+              sensorName: sensor.name,
+              value: sensor.value,
+              state: sensor.state
+            });
+
+            // Guardar el nuevo documento
+            await nuevoDocumento.save();
+
+
+          } catch (error) {
+
+          }
+        })();
+      });
       (async () => {
         try {
-          const nuevoDocumento = new Data({
-            timeStamp: fechaISO,
-            sensorName: sensor.name,
-            value: sensor.value,
-            state: sensor.state
-          });
-
-          // Guardar el nuevo documento
-          await nuevoDocumento.save();
-
-
+          await wsBroadcast(conexiones, ws, messageObj);
         } catch (error) {
-
+          console.error(error);
         }
       })();
-    });
-    (async () => {
-      try {
-        await wsBroadcast(conexiones, ws, messageObj);
-      } catch (error) {
-        console.error(error);
-      }
-    })();
+    }
   });
 
   //Websockets actions
 
   ws.on('message', (message) => {
-    console.log(JSON.parse(message));
-    client.publish('focoEsli/api/foco1', 'Mensaje desde front');
+    const publish = JSON.parse(message);
+    if (publish.type === 1) {
+      client.publish('focoEsli/api/foco1', 'Mensaje desde front');
+    } else if (publish.type === 2) {
+      client.publish('focoEsli/api/foco2', 'Mensaje desde front');
+    }
+
   });
 
   ws.on('close', () => {
